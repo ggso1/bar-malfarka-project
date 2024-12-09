49df4520f218887442f35c4065edff040d503bb4
ggso1 <24g_shsr@liceum.ztu.edu.ua>
2024-12-09 16:04:53 +0200

    adapt mob

--- js/burger.js
+++ js/burger.js
@@ -1,4 +1,14 @@
-function toggleMenu() {
-  const mobileMenu = document.getElementById("mobile-menu");
-  mobileMenu.style.display = mobileMenu.style.display === "flex" ? "none" : "flex";
-}
+// Отримання елементів
+const burgerButton = document.querySelector('.burger-button');
+const modalMenu = document.querySelector('.modal-menu');
+const closeButton = document.querySelector('.close-button');
+
+// Відкрити модальне меню
+burgerButton.addEventListener('click', () => {
+  modalMenu.classList.add('active'); // Показуємо модальне меню
+});
+
+// Закрити модальне меню
+closeButton.addEventListener('click', () => {
+  modalMenu.classList.remove('active'); // Ховаємо модальне меню
+});
