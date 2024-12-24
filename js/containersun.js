1bb56e40ca7d600197ed190a0089bdf0a16405ba
ggso1 <24g_shsr@liceum.ztu.edu.ua>
2024-12-24 14:58:26 +0200

    adapt

---
+++ js/containersun.js
@@ -0,0 +1,25 @@
+// Робимо функцію toggleForm глобальною
+function toggleForm() {
+  const formContainer = document.getElementById('formContainer');
+  if (formContainer) {
+    if (formContainer.style.display === 'block') {
+      formContainer.style.display = 'none';
+      console.log('Форма прихована');
+    } else {
+      formContainer.style.display = 'block';
+      console.log('Форма відкрита');
+    }
+  } else {
+    console.error('formContainer не знайдено');
+  }
+}
+
+// Робимо функцію confirmName глобальною
+function confirmName() {
+  const username = document.getElementById('username').value;
+  if (username) {
+    alert(`Привіт, ${username}!`);
+  } else {
+    alert('Будь ласка, введіть своє ім\'я!');
+  }
+}
